cfd0fc4d0f63620c648252108a8f563637d421a9
JaredTeo31 <97490330+JaredTeo31@users.noreply.github.com>
2022-03-15 10:53:13 +0800

    Update CheckingAccount.java

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -6,10 +6,27 @@ public class CheckingAccount {
         this.balance=balance;
         this.number=number;
     }
+    public void setBalance(double amount) throws IllegalArgumentException{
+        if (amount <= 0){
+            throw new IllegalArgumentException("Amount cannot be negative");}
+        else{
+            this.balance=amount;
+        }
+    }
+    public void setAccount(int number) throws IllegalArgumentException{
+        if (number <= 0){
+            throw new IllegalArgumentException("account number cannot be negative");}
+        else{
+            this.number=number;
+        }
+
+    }
     public void deposit(double amount){
         this.balance+=amount;
     }
-    public boolean withdraw(double amount) throws InsufficientFundsException{
+    public boolean withdraw(double amount) throws InsufficientFundsException,IllegalArgumentException{
+        if (amount <= 0){
+            throw new IllegalArgumentException("Withdraw amount cannot be negative");}
         if (this.balance < amount || amount <= 0) {
             double shortBy = amount -this.balance;
             //System.out.println("Sorry, but your account is short by: $" + shortBy);
